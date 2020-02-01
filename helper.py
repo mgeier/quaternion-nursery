d0ef4e5ebaa1beecbd16b1ff726b95e4c9c2192d
Matthias Geier <Matthias.Geier@gmail.com>
2020-02-01 12:15:42 +0100

    Change L to F

--- helper.py
+++ helper.py
@@ -1,23 +1,59 @@
+import numpy as np
 
 
 def faces():
-    yield [[1, 1, 1], [-1, 1, 1], [-1, -1, 1], [1, -1, 1]]
-    yield [[1, 1, -1], [1, -1, -1], [-1, -1, -1], [-1, 1, -1]]
-    yield [[-1, -1, 1], [-1, 1, 1], [-1, 1, -1], [-1, -1, -1]]
-    yield [[-1, -1, -1], [1, -1, -1], [1, -1, 1], [-1, -1, 1]]
-    for segment in range(4):
-        y1 = 1 + segment * 2
-        y2 = 3 + segment * 2
-        yield [[1, y1, 1], [1, y2, 1], [-1, y2, 1], [-1, y1, 1]]
-        yield [[1, y1, -1], [1, y2, -1], [1, y2, 1], [1, y1, 1]]
-        yield [[-1, y1, -1], [-1, y2, -1], [1, y2, -1], [1, y1, -1]]
-        yield [[-1, y1, 1], [-1, y2, 1], [-1, y2, -1], [-1, y1, -1]]
-    yield [[1, y2, 1], [1, y2, -1], [-1, y2, -1], [-1, y2, 1]]
-    for segment in range(2):
-        x1 = 1 + segment * 2
-        x2 = 3 + segment * 2
-        yield [[x1, -1, 1], [x2, -1, 1], [x2, 1, 1], [x1, 1, 1]]
-        yield [[x1, -1, -1], [x2, -1, -1], [x2, -1, 1], [x1, -1, 1]]
-        yield [[x1, 1, -1], [x2, 1, -1], [x2, -1, -1], [x1, -1, -1]]
-        yield [[x1, 1, 1], [x2, 1, 1], [x2, 1, -1], [x1, 1, -1]]
-    yield [[x2, -1, 1], [x2, -1, -1], [x2, 1, -1], [x2, 1, 1]]
+    """Quadratic faces for an F-shaped object."""
+    top = np.array([[1, 1, 1], [-1, 1, 1], [-1, -1, 1], [1, -1, 1]])
+    north = np.array([[1,  1, -1], [-1,  1, -1], [-1,  1,  1], [1,  1,  1]])
+    east = np.array([[1,  1, -1], [1,  1,  1], [1, -1,  1], [1, -1, -1]])
+    south = np.array([[1, -1,  1], [-1, -1,  1], [-1, -1, -1], [1, -1, -1]])
+    west = np.array([[-1,  1,  1], [-1,  1, -1], [-1, -1, -1], [-1, -1,  1]])
+    bottom = np.array([[-1,  1, -1], [1,  1, -1], [1, -1, -1], [-1, -1, -1]])
+    origin = np.array([0, 0, 0])
+    p = origin + [0, 0, 0]
+    yield p + top
+    yield p + west
+    yield p + bottom
+    p += [2, 0, 0]
+    yield p + top
+    yield p + north
+    yield p + bottom
+    yield p + south
+    p += [2, 0, 0]
+    yield p + top
+    yield p + north
+    yield p + bottom
+    yield p + south
+    yield p + east
+    p = origin + [0, 2, 0]
+    yield p + top
+    yield p + west
+    yield p + bottom
+    yield p + east
+    p += [0, 2, 0]
+    yield p + top
+    yield p + west
+    yield p + bottom
+    yield p + north
+    p += [2, 0, 0]
+    yield p + top
+    yield p + north
+    yield p + bottom
+    yield p + south
+    p += [2, 0, 0]
+    yield p + top
+    yield p + north
+    yield p + bottom
+    yield p + south
+    yield p + east
+    p = origin + [0, -2, 0]
+    yield p + top
+    yield p + west
+    yield p + bottom
+    yield p + east
+    p += [0, -2, 0]
+    yield p + top
+    yield p + west
+    yield p + bottom
+    yield p + east
+    yield p + south
