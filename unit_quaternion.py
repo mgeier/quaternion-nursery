05b256e9d0186cdf3ec71b467c86c5ca29ba96cb
Matthias Geier <Matthias.Geier@gmail.com>
2020-04-16 10:31:23 +0200

    Remove Vector class

--- unit_quaternion.py
+++ unit_quaternion.py
@@ -2,100 +2,6 @@ from collections import namedtuple as _namedtuple
 import math as _math
 
 
-class Vector(_namedtuple('VectorBase', 'x y z')):
-    """Three-dimensional vector (named tuple).
-
-    See `collections.namedtuple`.
-
-    Attributes
-    ----------
-    x : float
-        x coordinate.
-    y : float
-        y coordinate.
-    z : float
-        z coordinate.
-
-    """
-
-    __slots__ = ()
-
-    def __neg__(self):
-        return Vector(
-            -self.x,
-            -self.y,
-            -self.z,
-        )
-
-    def __mul__(self, scalar):
-        return Vector(
-            self.x * scalar,
-            self.y * scalar,
-            self.z * scalar,
-        )
-
-    def __rmul__(self, scalar):
-        return self.__mul__(scalar)
-
-    def __truediv__(self, scalar):
-        return Vector(
-            self.x / scalar,
-            self.y / scalar,
-            self.z / scalar,
-        )
-
-    #def __xor__(self, other):
-    #    """Cross product."""
-    #    a1, a2, a3 = self
-    #    b1, b2, b3 = other
-    #    return Vector(
-    #        a2 * b3 - a3 * b2,
-    #        a3 * b1 - a1 * b3,
-    #        a1 * b2 - a2 * b1,
-    #    )
-
-    #def __rxor__(self, other):
-    #    # TODO: anti-commutative?
-    #    return NotImplemented
-
-    #def __matmul__(self, other):
-    #    """Dot product."""
-    #    a1, a2, a3 = self
-    #    b1, b2, b3 = other
-    #    return (a1 * b1) + (a2 * b2) + (a3 * b3)
-
-    #def __rmatmul__(self, other):
-    #    return self.__matmul__(other)
-
-    @property
-    def norm(self):
-        return _math.sqrt(self.x**2 + self.y**2 + self.z**2)
-
-    def normalized(self):
-        # TODO: return UnitVector?
-        return self / self.norm
-
-
-def ensure_vector(v):
-    if not isinstance(v, Vector):
-        v = Vector(*v)
-    return v
-
-
-#class UnitVector(Vector):
-#
-#    __slots__ = ()
-#
-#    def __new__(cls):
-#        raise TypeError('Use UnitVector.from_vector() to create a UnitVector')
-#
-#    @classmethod
-#    def from_vector(cls, v):
-#        v = ensure_vector(v)
-#        norm = v.norm
-#        return super().__new__(cls, v.x / norm, v.y / norm, v.z / norm)
-
-
 class Quaternion(_namedtuple('QuaternionBase', 'scalar vector')):
 
     __slots__ = ()
@@ -115,7 +21,7 @@ class Quaternion(_namedtuple('QuaternionBase', 'scalar vector')):
             result_type,
             # TODO: properly derive this
             a1*a2 - b1*b2 - c1*c2 - d1*d2,
-            Vector(
+            (
                 a1*b2 + b1*a2 + c1*d2 - d1*c2,
                 a1*c2 - b1*d2 + c1*a2 + d1*b2,
                 a1*d2 + b1*c2 - c1*b2 + d1*a2,
@@ -127,10 +33,11 @@ class Quaternion(_namedtuple('QuaternionBase', 'scalar vector')):
         return NotImplemented
 
     def conjugate(self):
+        x, y, z = self.vector
         return super().__new__(
             type(self),
             self.scalar,
-            -ensure_vector(self.vector),
+            (-x, -y, -z),
         )
 
     @property
@@ -158,15 +65,19 @@ class UnitQuaternion(Quaternion):
 
         """
         # TODO: implement with exponential map?
+        x, y, z = axis
+        norm = _math.sqrt(x**2 + y**2 + z**2)
+        s = _math.sin(angle / 2)
         return super().__new__(
             cls,
             _math.cos(angle / 2),
-            _math.sin(angle / 2) * Vector(*axis).normalized(),
+            (s * x / norm, s * y / norm, s * z / norm),
         )
 
     def __pow__(self, alpha):
         return UnitQuaternion.from_axis_angle(self.axis, alpha * self.angle)
 
+    # TODO: proper implementation to get meaningful docstring?
     inverse = Quaternion.conjugate
 
     # TODO: exponential map
@@ -175,8 +86,10 @@ class UnitQuaternion(Quaternion):
     @property
     def axis(self):
         assert self.scalar <= 1
-        # NB: This is the same as self.vector.normalized()
-        return ensure_vector(self.vector) / _math.sqrt(1 - self.scalar**2)
+        # NB: This is the same as sqrt(x**2 + y**2 + z**2)
+        norm = _math.sqrt(1 - self.scalar**2)
+        x, y, z = self.vector
+        return x / norm, y / norm, z / norm
 
     @property
     def angle(self):
